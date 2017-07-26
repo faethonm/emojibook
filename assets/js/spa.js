29eb6cd5c3ce8d624b05dd5b5deb105617373246
Faethon Milikouris <faethon.milikouris@gmail.com>
2017-07-26 00:30:22 -0700

    Initial commit

---
+++ assets/js/spa.js
@@ -0,0 +1,20 @@
+angular.module('EmojiBook', []);
+angular.module('EmojiBook').controller(
+  'BaseCtrl',
+  ['$scope', function($scope) {
+
+    io.socket.get('/emoji', function(data) {
+      $scope.emojis = data;
+      $scope.$apply();
+    })
+
+    io.socket.on('emoji', function(event) {
+      switch (event.verb) {
+        case 'created':
+          $scope.emojis.push(event.data)
+          $scope.$apply();
+          break;
+      }
+    })
+  }]
+);
